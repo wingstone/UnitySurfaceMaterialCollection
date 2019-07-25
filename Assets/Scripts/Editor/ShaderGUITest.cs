207477543a70df03d2c83d8320288099777b6b58
wingstone <1812467051@qq.com>
2019-07-26 00:02:10 +0800

    add shadergui

---
+++ Assets/Scripts/Editor/ShaderGUITest.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ShaderGUITest : ShaderGUI
+{
+    public enum SurfaceType
+    {
+        Opaque,
+        Transparent
+    }
+
+    public enum BlendMode
+    {
+        Alpha,
+        Premultiply,
+        Additive,
+        Multiply
+    }
+
+    public enum CullMode
+    {
+        CullBack,
+        CullFront,
+        CullOff
+    }
+
+    bool firstApply = true;
+
+    MaterialProperty alpha;
+    MaterialProperty surface;
+    MaterialProperty blend;
+    MaterialProperty cull;
+    MaterialProperty zwrite;
+    MaterialProperty srcblend;
+    MaterialProperty dstblend;
+
+    public void MaterialChanged(MaterialEditor materialEditor)
+    {
+        Material material = materialEditor.target as Material;
+
+        SurfaceType surfaceType = (SurfaceType)material.GetInt("_SurfaceType");
+        if (surfaceType == SurfaceType.Opaque)
+        {
+            material.SetOverrideTag("RenderType", "Opaque");
+            material.SetInt("_ZWrite", 1);
+            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
+        }
+        else
+        {
+            BlendMode blendMode = (BlendMode)material.GetInt("_BlendMode");
+            switch (blendMode)
+            {
+                case BlendMode.Alpha:
+                    material.SetOverrideTag("RenderType", "Transparent");
+                    material.SetInt("_ZWrite", 0);
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                    break;
+                case BlendMode.Premultiply:
+                    material.SetOverrideTag("RenderType", "Transparent");
+                    material.SetInt("_ZWrite", 0);
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                    break;
+                case BlendMode.Additive:
+                    material.SetOverrideTag("RenderType", "Transparent");
+                    material.SetInt("_ZWrite", 0);
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                    break;
+                case BlendMode.Multiply:
+                    material.SetOverrideTag("RenderType", "Transparent");
+                    material.SetInt("_ZWrite", 0);
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.DstColor);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        //base.OnGUI(materialEditor, properties);
+
+        Material material = materialEditor.target as Material;
+
+        alpha = FindProperty("_Alpha", properties);
+        surface = FindProperty("_SurfaceType", properties);
+        blend = FindProperty("_BlendMode", properties);
+        cull = FindProperty("_Cull", properties);
+        zwrite = FindProperty("_ZWrite", properties);
+        srcblend = FindProperty("_SrcBlend", properties);
+        dstblend = FindProperty("_DstBlend", properties);
+
+        if (firstApply)
+        {
+            MaterialChanged(materialEditor);
+            firstApply = false;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        {
+            EditorGUI.BeginChangeCheck();
+            var type = EditorGUILayout.Popup("Surface Type", (int)surface.floatValue, Enum.GetNames(typeof(SurfaceType)));
+            if (EditorGUI.EndChangeCheck())
+            {
+                surface.floatValue = (float)type;
+            }
+
+            EditorGUI.BeginChangeCheck();
+            var blendMode = EditorGUILayout.Popup("Blend Mode", (int)blend.floatValue, Enum.GetNames(typeof(BlendMode)));
+            if (EditorGUI.EndChangeCheck())
+            {
+                blend.floatValue = (float)blendMode;
+            }
+
+            EditorGUI.BeginChangeCheck();
+            var cullMode = EditorGUILayout.Popup("Cull Mode", (int)cull.floatValue, Enum.GetNames(typeof(UnityEngine.Rendering.CullMode)));
+            if (EditorGUI.EndChangeCheck())
+            {
+                cull.floatValue = (float)cullMode;
+            }
+        }
+        if(EditorGUI.EndChangeCheck())
+        {
+            MaterialChanged(materialEditor);
+        }
+        
+
+        materialEditor.RangeProperty(alpha, "Alpha Range");
+    }
+}
---
+++ Assets/Shaders/ShaderGUITest.shader
@@ -0,0 +1,73 @@
+﻿Shader "Custom/ShaderGUITest"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Alpha("Alpha", Range(0,1)) = 1.0
+		_SurfaceType("SurfaceType", Int) = 0
+		_BlendMode ("BlendMode", Int) = 0
+		_Cull("__cull", Int) = 0
+		_ZWrite("__zwrite", Int) = 1
+		_SrcBlend("__srcblend", Int) = 1
+		_DstBlend("__dstblend", Int) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+		Cull [_Cull]
+		ZWrite [_ZWrite]
+		Blend [_SrcBlend] [_DstBlend]
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			half _Alpha;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+				col.a = _Alpha;
+                return col;
+            }
+            ENDCG
+        }
+    }
+
+	CustomEditor "ShaderGUITest"
+}
