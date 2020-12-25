f8a2e67acdf00aadcf17eef9d19cc4fde3ac4e0a
wingstone <1812467051@qq.com>
2020-12-25 23:50:12 +0800

    add plane shadow

---
+++ Assets/PlaneShadow/planeShadow.shader
@@ -0,0 +1,104 @@
+﻿Shader "ShadingModel/planeShadow"
+{
+    Properties
+    {
+        _Color("Main Color", Color) = (0,0,0,1)
+        _Height("Height", Float) = 0.0
+        _Delt("Delt", Float) = 0.001
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque+1" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Lighting.cginc"
+            #include "AutoLight.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 normal : NORMAL;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float3 normal : TEXCOORD0;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.normal = UnityObjectToWorldNormal(v.normal.xyz);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float3 L = _WorldSpaceLightPos0.xyz;
+                float3 N = normalize(i.normal);
+
+                float NoL = saturate(dot(L, N));
+                float3 col = _LightColor0.rgb * NoL;
+
+                return float4(col, 1);
+            }
+            ENDCG
+        }
+
+        Pass
+        {
+            Tags {"LightMode" = "ForwardBase"}
+
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            float _Height;
+            float _Delt;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float4 worldPos = mul(UNITY_MATRIX_M, v.vertex);
+                worldPos /= worldPos.w;
+                float3 L = _WorldSpaceLightPos0.xyz;
+                worldPos.xyz += (_Height - worldPos.y)*L*rcp(L.y);
+                worldPos.y += _Delt;
+
+                o.vertex = mul(UNITY_MATRIX_VP, worldPos);
+                return o;
+            }
+
+            half4 _Color;
+
+            half4 frag (v2f i) : SV_Target
+            {
+                return _Color;
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/Tools/CubeMapConvertWindow.cs
+++ Assets/Tools/CubeMapConvertWindow.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEditor;
 using System.IO;
@@ -35,24 +35,19 @@ namespace Tools
 
         public void OnGUI()
         {
-            // texture
-            // srcTex = EditorGUILayout.ObjectField(srcTex, typeof(Texture2D), false) as Texture2D;
-            // toCubeshader = EditorGUILayout.ObjectField(toCubeshader, typeof(Shader), false) as Shader;
-            // if (toCubeshader)
-            //     toCubematerial = new Material(toCubeshader);
-
-            // cubemap = new Cubemap(256, TextureFormat.RGBA32, false);
-
-
+            srcTex = EditorGUILayout.ObjectField(srcTex, typeof(Texture2D), false) as Texture2D;
+            toCubeshader = EditorGUILayout.ObjectField(toCubeshader, typeof(Shader), false) as Shader;
+            if (toCubeshader && !toCubematerial)
+                toCubematerial = new Material(toCubeshader);
 
             cubemap = EditorGUILayout.ObjectField(cubemap, typeof(Cubemap), false) as Cubemap;
             toMatcapshader = EditorGUILayout.ObjectField(toMatcapshader, typeof(Shader), false) as Shader;
-            if (toMatcapshader)
+            if (toMatcapshader && !toMatcapmaterial)
                 toMatcapmaterial = new Material(toMatcapshader);
 
             if (GUILayout.Button("Convert To Matcap"))
             {
-                for (int i = 0; i <= 10; i++)
+                for (int i = 1; i <= 10; i++)
                 {
                     int width = 1024 >> i;
                     RenderTexture rt = RenderTexture.GetTemporary(width, width, 0, RenderTextureFormat.ARGB32);
@@ -60,7 +55,6 @@ namespace Tools
                     toMatcapmaterial.SetFloat("_LOD", i);
                     toMatcapmaterial.SetFloat("_SrcMipmapCount", cubemap.mipmapCount);
                     toMatcapmaterial.SetFloat("_OmegaPInv", 6.0f * cubemap.width * cubemap.width / (4f * Mathf.PI));
-                    // Debug.Log(cubemap.width);
 
                     Graphics.Blit(rt, rt, toMatcapmaterial, 0);
 
@@ -70,29 +64,74 @@ namespace Tools
                     tex.Apply();
                     RenderTexture.ReleaseTemporary(rt);
 
-                    string path = AssetDatabase.GetAssetPath(cubemap) + "_Matcap" + i + ".png";
+                    string path = AssetDatabase.GetAssetPath(cubemap) + "_Matcap" + "_mip" + i + ".png";
                     File.WriteAllBytes(path, tex.EncodeToPNG());
-                    AssetDatabase.Refresh();
-                    TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
-                    importer.textureType = TextureImporterType.Default;
-                    importer.SaveAndReimport();
+
                 }
-            }
-
-            if (GUILayout.Button("Convert To CrossLayout"))
-            {
-                RenderTexture rt = RenderTexture.GetTemporary(1024, 1024, 0, RenderTextureFormat.ARGB32);
-                toMatcapmaterial.SetTexture("_Cubemap", cubemap);
-                Graphics.Blit(rt, rt, toMatcapmaterial, 3);
-
-                var tex = new Texture2D(256 * 6, 256);
-                RenderTexture.active = rt;
-                tex.ReadPixels(new Rect(0, 0, 256 * 6, 256), 0, 0);
-                tex.Apply();
-                File.WriteAllBytes(AssetDatabase.GetAssetPath(cubemap) + "_CrossLayout.png", tex.EncodeToPNG());
 
                 AssetDatabase.Refresh();
+                for (int i = 1; i <= 10; i++)
+                {
+                    string path = AssetDatabase.GetAssetPath(cubemap) + "_Matcap" + "_mip" + i + ".png";
+                    TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
+                    importer.textureType = TextureImporterType.Default;
+                    importer.mipmapEnabled = false;
+                    importer.isReadable = true;
+                    importer.SaveAndReimport();
+                }
+
+                //mip 0
+                {
+                    int width = 1024;
+                    RenderTexture rt = RenderTexture.GetTemporary(width, width, 0, RenderTextureFormat.ARGB32);
+                    toMatcapmaterial.SetTexture("_Cubemap", cubemap);
+                    toMatcapmaterial.SetFloat("_LOD", 0);
+                    toMatcapmaterial.SetFloat("_SrcMipmapCount", cubemap.mipmapCount);
+                    toMatcapmaterial.SetFloat("_OmegaPInv", 6.0f * cubemap.width * cubemap.width / (4f * Mathf.PI));
+
+                    Graphics.Blit(rt, rt, toMatcapmaterial, 0);
+
+                    var tex = new Texture2D(width, width);
+                    RenderTexture.active = rt;
+                    tex.ReadPixels(new Rect(0, 0, width, width), 0, 0);
+                    tex.Apply();
+                    RenderTexture.ReleaseTemporary(rt);
+
+                    string path = AssetDatabase.GetAssetPath(cubemap) + "_Matcap" + "_mip" + ".png";
+                    File.WriteAllBytes(path, tex.EncodeToPNG());
+
+                }
+
+                AssetDatabase.Refresh();
+                {
+                    string path = AssetDatabase.GetAssetPath(cubemap) + "_Matcap" + "_mip" + ".png";
+                    TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
+                    importer.textureType = TextureImporterType.Default;
+                    importer.mipmapEnabled = true;
+                    importer.isReadable = true;
+                    importer.SaveAndReimport();
+                }
+
             }
+
+            // srcTex = EditorGUILayout.ObjectField(srcTex, typeof(Texture2D), false) as Texture2D;
+
+            // if (GUILayout.Button("Convert To Matcap"))
+            // {
+            //     RenderTexture ret = RenderTexture.GetTemporary(1024, 1024, 0, RenderTextureFormat.ARGB32);
+            //     RenderTexture reet = RenderTexture.GetTemporary(1024, 1024, 0, RenderTextureFormat.ARGB32);
+            //     Graphics.Blit(srcTex, ret);
+            //     Graphics.CopyTexture(ret, reet);
+
+            //     var teex = new Texture2D(1024, 1024);
+            //     RenderTexture.active = reet;
+            //     teex.ReadPixels(new Rect(0, 0, 1024, 1024), 0, 0);
+            //     teex.Apply();
+            //     RenderTexture.ReleaseTemporary(ret);
+            //     RenderTexture.ReleaseTemporary(reet);
+            //     string pah = AssetDatabase.GetAssetPath(cubemap) + "_test" + ".png";
+            //     File.WriteAllBytes(pah, teex.EncodeToPNG());
+            // }
         }
 
     }
